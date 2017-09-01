e54ff9a80b8aa06660ad58fc7d5740efd84cbbd9
BK <bkam03@yahoo.com>
2017-08-31 20:38:44 -1000

    added test data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import './index.css';
+import cards from './reducers';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore();
+const store = createStore( cards );
 
 ReactDOM.render(
   <Provider store={store}>
