5611b3bd6edaa8ac1815b52e2ca75e578abd75e0
BK <bkam03@yahoo.com>
2017-08-31 20:38:06 -1000

    added CardList component to render

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addCard } from '../../actions';
+import CardList from '../../components/CardList.js';
 
 class App extends Component {
   render() {
+    console.log( 'props', this.props);
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <CardList
+        cards= { this.props.cards }
+      />
     );
   }
 }
+//transfers store info to props
+const mapStateToProps = ( state ) => {
+  return {
+    cards: state.cards
+  };
+};
+//this allows components to dispatch actions to reducer
+const mapDispatchToProps = ( dispatch ) => {
+  return {//when you call this.props.addCard, its calling this.
+    addCard: (card) => {
+      dispatch(addCard(card))
+    }
+  };
+};
 
-export default App;
+const ConnectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default ConnectedApp;
