e88131c11da6d11ed1f10b3e1e2af9995c586d43
BK <bkam03@yahoo.com>
2017-09-02 13:43:38 -1000

    fixed render so it feeds right cards to card component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Card from './Card.js';
 
 const CardList = ( {cards} ) => {
-  console.log( 'CardList component', cards );
+  console.log( 'CardList component cards.cards', cards);
   return (
     <ul>
-
-      {cards.map( (card) => {
+      {cards.cards.map( (card) => {
         return (
          <Card
           card={card}
