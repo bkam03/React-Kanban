2512c6224b046073471f42b06ae9c6f4f082d03a
BK <bkam03@yahoo.com>
2017-09-02 18:34:44 -1000

    removed user option

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 export const ADD_CARD = 'ADD_CARD';
 export const MOVE_CARD = 'MOVE_CARD';
-export const ADD_USER = 'ADD_USER';
 
 
 export const addCard = (card) => {
@@ -8,15 +7,5 @@ export const addCard = (card) => {
   return {
     type: ADD_CARD,
     card: card
-  };
-};
-
-
-
-export const addUser = (user) => {
-  console.log( 'addUser action', user );
-  return {
-    type: ADD_USER,
-    user: user
   };
 };
