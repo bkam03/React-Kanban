31f0b24b2c4d088d35d77aafe268755016a6713f
BK <bkam03@yahoo.com>
2017-09-02 15:57:48 -1000

    added more test data to see if cou lumns filtered properly

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,13 +1,29 @@
 import { ADD_CARD, ADD_USER } from '../actions';
 
 const initialState = {
-  cards : [{
-    title: "test",
-    priority: "none",
-    status: "Queue",
-    createdBy: "me",
-    assignedTo: "you"
-  }]
+  cards : [
+    {
+      title: "test",
+      priority: "none",
+      status: "Queue",
+      createdBy: "me",
+      assignedTo: "you"
+    },
+    {
+      title: "2",
+      priority: "low",
+      status: "In Progress",
+      createdBy: "me",
+      assignedTo: "you"
+    },
+    {
+      title: "test3",
+      priority: "none",
+      status: "Complete",
+      createdBy: "me",
+      assignedTo: "you"
+    }
+  ]
 }; //make array blank later
 
 const cards = ( state = initialState, action ) => {
