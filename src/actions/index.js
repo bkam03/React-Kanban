c0c3ce167c835ece97a8fb0337270e8cfec30c47
BK <bkam03@yahoo.com>
2017-09-05 20:21:39 -1000

    incorporated react-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,22 @@
+import { getCardsFromDB, addCardToDB } from '../lib/fakedb.js';
+
 export const ADD_CARD = 'ADD_CARD';
 export const ADVANCE_CARD = 'ADVANCE_CARD';
 export const GET_CARDS = 'GET_CARDS';
 
-import { getCardsFromDB, addCardToDB } from '../lib/fakedb.js';
 
 export const getCards = ( cards ) => {
-  return( ( dispatch ) => {
+  console.log( 'firing getCards action' );
+  return ( dispatch ) => {
     return getCardsFromDB()
       .then( ( cards ) => {
-        dispatch {
+        console.log( 'From db', cards );
+        dispatch ( {
           type: GET_CARDS,
           cards: cards
-        }
-      } )
-  } );
+        } );
+      } );
+  }
 };
 
 export const addCard = (card) => {
