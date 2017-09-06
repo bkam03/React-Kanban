1b21f4fed99f8d320d72821e6fc854f38fbd00a9
BK <bkam03@yahoo.com>
2017-09-05 20:22:38 -1000

    incorporated react-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
 
 import './index.css';
 import reducers from './reducers';
