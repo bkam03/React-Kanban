e5898202bb678c02a6479f892774798a22e6347e
BK <bkam03@yahoo.com>
2017-09-05 17:57:10 -1000

    set up redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import './index.css';
 import reducers from './reducers';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore( reducers );
+const store = createStore(
+  reducers,
+  applyMiddleware( ReduxThunk )
+);
 
 ReactDOM.render(
   <Provider store={store}>
