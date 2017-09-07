e1bca2faaf00c947ba8bb767b40568ff68b5ccbb
BK <bkam03@yahoo.com>
2017-09-06 21:13:05 -1000

    basic routes respond to postman

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,9 +2,25 @@ const express = require( 'express' );
 
 const router = express.Router();
 
-router.get( '/', ( req, res ) => {
-  console.log( 'getting from server' );
+router.post( '/createcard', ( req, res ) => {
+  console.log( 'create card' );
   res.end();
 } );
+
+router.get( '/readcards', ( req, res ) => {
+  console.log( 'read cards' );
+  res.end();
+} );
+
+router.put( '/updatecard', ( req, res ) => {
+  console.log( 'update card' );
+  res.end();
+} );
+
+router.delete( '/deletecard', ( req, res ) => {
+  console.log( 'delete card' );
+  res.end();
+} );
+
 
 module.exports = router;
