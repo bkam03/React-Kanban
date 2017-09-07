15bd69d508c4d1f13c3a836b8b129e732e7fb141
BK <bkam03@yahoo.com>
2017-09-06 21:03:31 -1000

    successfully connected router, get route responds

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require( 'express' );
 
+const routes = require( './routes/routes.js' );
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use( '/', routes );
 
 const server = app.listen( PORT, () => {
   console.log( `server running on ${ PORT }`);
