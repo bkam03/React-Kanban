1ceb7ab266ee0a7a785fe54735e78e9970411b0b
BK <bkam03@yahoo.com>
2017-09-07 18:00:57 -1000

    added body parser dependencies

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require( 'express' );
+const bp = require( 'body-parser' );
 
 const routes = require( './routes/routes.js' );
-const db = require( './models' );
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use( bp.urlencoded() );
 
 app.use( '/', routes );
 
