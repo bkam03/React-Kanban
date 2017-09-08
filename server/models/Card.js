b0b413e5380733aa58b2ab760c943646e6998f84
BK <bkam03@yahoo.com>
2017-09-07 17:59:54 -1000

    added card field data types

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,6 +1,10 @@
 module.exports = function( sequelize, DataTypes ) {
   var Card = sequelize.define( "Card", {
-
+    title: DataTypes.STRING,
+    status: DataTypes.STRING,
+    priority: DataTypes.STRING,
+    createdBy: DataTypes.STRING,
+    assignedTo: DataTypes.STRING
 
   }, {
     classMethods: {
