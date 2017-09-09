3de1b9324a70af8039791588f64051d980012f81
BK <bkam03@yahoo.com>
2017-09-09 09:35:43 -1000

    created regress card action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import { getCardsFromServer, addCardToServer } from '../lib/cards-api.js';
 export const ADD_CARD = 'ADD_CARD';
 export const ADVANCE_CARD = 'ADVANCE_CARD';
 export const GET_CARDS = 'GET_CARDS';
-
+export const REGRESS_CARD = 'REGRESS_CARD';
 
 export const getCards = () => {
   return ( dispatch ) => {
@@ -38,6 +38,14 @@ export const advanceCard = ( card ) => {
   console.log( 'advance card', card );
   return {
     type: ADVANCE_CARD,
+    card: card
+  }
+}
+
+export const regressCard = ( card ) => {
+  console.log( 'regress card', card );
+  return {
+    type: REGRESS_CARD,
     card: card
   }
 }
