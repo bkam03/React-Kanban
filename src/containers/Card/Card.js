5b5abad5869a653a14360584a55021de47567dab
BK <bkam03@yahoo.com>
2017-09-09 09:36:48 -1000

    added regress card action

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { advanceCard } from '../../actions/';
+import { advanceCard, regressCard } from '../../actions/';
 
 class Card extends Component {
 
@@ -8,22 +8,31 @@ class Card extends Component {
     console.log( "handleTaskAdvance", this.props.card );
     this.props.advanceCard( this.props.card );
   }
+  handleTaskRegress(){
+    console.log( "handleTaskRegress", this.props.card );
+    this.props.regressCard( this.props.card );
+  }
 
   render(){
     let card = this.props.card;
     return (
       <div>
-        Title: {card.title}
-        <br />
-        Priority: {card.priority}
-        <br />
-        Status: {card.status}
-        <br />
-        createdBy: {card.createdBy}
-        <br />
-        assignedTo: {card.assignedTo}
-        <button type="button" onClick={this.handleTaskAdvance.bind(this)}>next</button>
-        <br />
+        <div>
+          Title: {card.title}
+          <br />
+          Priority: {card.priority}
+          <br />
+          Status: {card.status}
+          <br />
+          createdBy: {card.createdBy}
+          <br />
+          assignedTo: {card.assignedTo}
+          <br />
+        </div>
+        <div>
+          <button type="button" onClick={this.handleTaskRegress.bind(this)}>back</button>
+          <button type="button" onClick={this.handleTaskAdvance.bind(this)}>next</button>
+        </div>
       </div>
     );
   }
@@ -33,7 +42,11 @@ const mapDispatchToProps = ( dispatch ) => {
   return {
     advanceCard: ( card ) => {
       console.log( 'dispatch card', card );
-      dispatch( advanceCard( card ) )
+      dispatch( advanceCard( card ) );
+    },
+    regressCard: ( card ) => {
+      console.log( 'dispatch card', card );
+      dispatch( regressCard( card ) );
     }
   };
 };
