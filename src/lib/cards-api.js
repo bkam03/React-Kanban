f532b8321124d807aa0c8933bb9fe8db76448e85
BK <bkam03@yahoo.com>
2017-09-09 14:05:17 -1000

    processes server return from json to objct

--- src/lib/cards-api.js
+++ src/lib/cards-api.js
@@ -14,9 +14,14 @@ export const getCardsFromServer = () => {
 export const addCardToServer = ( card ) => {
   return new Promise( ( resolve, reject ) => {
     let xhrRequest = new XMLHttpRequest();
+
     xhrRequest.addEventListener( 'load', function(){
-      resolve();
+      console.log( 'returned from server', this.response );
+      let newCard = JSON.parse( this.response ).card;
+      console.log( 'after processing', newCard );
+      resolve( newCard );
     } );
+
     xhrRequest.open( 'POST', '/createcard', true );
     xhrRequest.setRequestHeader("Content-Type", "application/json");
     xhrRequest.send( JSON.stringify(card) );
@@ -24,6 +29,7 @@ export const addCardToServer = ( card ) => {
 };
 
 export const editCardInServer = ( card ) => {
+  console.log( 'edit card xml call start')
   return new Promise( ( resolve, reject ) => {
     let xhrRequest = new XMLHttpRequest();
     xhrRequest.addEventListener( 'load', function(){
